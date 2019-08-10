e1db4f7236ceee650a678f078b434fc20bae5a40
Eddie Stanley <acklavidian@gmail.com>
2019-08-10 14:01:32 -0500

    reorg files

--- Eyes.gd
+++
@@ -1,9 +0,0 @@
-extends CPUParticles2D
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-onready var Character =  get_node('../../2D3D-Sprite')
-onready var offset = position
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-    Character.move_node2D_to_bone(self,Character.Bone.HEAD, Vector2(4, -4))    
